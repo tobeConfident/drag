ce12caf3a445e5b2569ea635e6fb622f717a903f
zheng_yq@suixingpay.com <zheng_yq@suixingpay.com>
2018-12-27 12:00:18 +0800

    可拖动、可伸缩

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import BasicLine from './page/BasicLine';
+import Drag from './page/Drag';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Drag />
+      </div>
+    );
+  }
+}
+
+export default App;
